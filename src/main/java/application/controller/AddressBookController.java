4991257d81ec4d64704b036d69b21057d63feb25
yiyuankafei <124633367@qq.com>
2019-11-12 18:42:03 +0800

    添加通讯录

--- src/main/java/application/controller/AddressBookController.java
+++ src/main/java/application/controller/AddressBookController.java
@@ -3,19 +3,25 @@ package application.controller;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.constant.CheckResult;
+import application.constant.ResEnv;
 import application.entity.Contact;
 import application.entity.ContactExample;
 import application.service.ContactService;
 import application.util.JwtUtils;
+import application.util.PinYinUtil;
 
 @RestController
 public class AddressBookController {
@@ -39,4 +45,29 @@ public class AddressBookController {
 		
 		return map;
 	}
+	
+	@PostMapping("/contact")
+	public ResEnv<Void> add(HttpServletRequest request, @RequestBody Contact contact) {
+		
+		String token = request.getHeader("token");
+		CheckResult checkResult = JwtUtils.validateJWT(token);
+		Integer userId = Integer.parseInt(checkResult.getClaims().getId());
+		
+		String pinyin = PinYinUtil.getPinYin(contact.getNickname());
+		String letterGroup = String.valueOf(pinyin.charAt(0)).toUpperCase();
+		
+		String reg = "[A-Z]";
+		Pattern compile = Pattern.compile(reg);
+		Matcher matcher = compile.matcher(letterGroup);
+		if (!matcher.matches()) {
+			letterGroup = "#";
+		}
+		
+		contact.setLetterGroup(letterGroup);
+		contact.setUserId(userId);
+		
+		contactService.insert(contact);
+		return ResEnv.success();
+	}
+	
 }
